437adfd5e75748f83e2e08b6ea7c9a9aa8f749b0
Nour <nourmuhamed01@gmail.com>
2022-04-07 21:53:36 +0200

    display palette registers

--- src/core/display.c
+++ src/core/display.c
@@ -52,6 +52,37 @@ void write_display_io(uint16_t addr, uint8_t byte) {
 #endif
         display.scy = byte;
         return;
+    case LY:
+#ifdef DISPLAY_LOG
+        write_log("[display] write to LY register, resetting...\n");
+#endif
+        display.ly = 0;
+        return;
+    case LYC:
+#ifdef DISPLAY_LOG
+        write_log("[display] write to LY register value 0x%02X\n", byte);
+#endif
+        display.lyc = byte;
+        return;
+    case BGP:
+#ifdef DISPLAY_LOG
+        write_log("[display] write to BGP register value 0x%02X\n", byte);
+#endif
+        display.bgp = byte;
+        return;
+    case OBP0:
+#ifdef DISPLAY_LOG
+        write_log("[display] write to OBP0 register value 0x%02X\n", byte);
+#endif
+        display.obp0 = byte;
+        return;
+    case OBP1:
+#ifdef DISPLAY_LOG
+        write_log("[display] write to OBP1 register value 0x%02X\n", byte);
+#endif
+        display.obp1 = byte;
+        return;
+
     default:
         write_log("[memory] unimplemented write to I/O port 0x%04X value 0x%02X\n", addr, byte);
         die(-1, NULL);
@@ -60,8 +91,27 @@ void write_display_io(uint16_t addr, uint8_t byte) {
 
 uint8_t read_display_io(uint16_t addr) {
     switch(addr) {
+    case LCDC:
+        return display.lcdc;
+    case STAT:
+        return display.stat;
+    case SCY:
+        return display.scy;
+    case SCX:
+        return display.scx;
     case LY:
         return display.ly;
+    case LYC:
+        return display.lyc;
+    case DMA:
+        write_log("[display] undefined read from write-only DMA register, returning ones\n");
+        return 0xFF;
+    case BGP:
+        return display.bgp;
+    case OBP0:
+        return display.obp0;
+    case OBP1:
+        return display.obp1;
     default:
         write_log("[memory] unimplemented read from IO port 0x%04X\n", addr);
         die(-1, NULL);
