abd75abc317bbff56a315eaa9adafb6a93898d1d
Nour <nourmuhamed01@gmail.com>
2022-04-14 19:50:36 +0200

    completed sound I/O writes

--- src/core/sound.c
+++ src/core/sound.c
@@ -56,6 +56,135 @@ void sound_write(uint16_t addr, uint8_t byte) {
         if(byte & 0x80) sound.nr52 |= 0x80;
         else sound.nr52 &= 0x7F;
         return;
+    case NR10:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR10 register value 0x%02X\n", byte);
+#endif
+        sound.nr10 = byte;
+        return;
+    case NR11:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR11 register value 0x%02X\n", byte);
+#endif
+        sound.nr11 = byte;
+        return;
+    case NR12:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR12 register value 0x%02X\n", byte);
+#endif
+        sound.nr12 = byte;
+        return;
+    case NR13:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR13 register value 0x%02X\n", byte);
+#endif
+        sound.nr13 = byte;
+        return;
+    case NR14:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR15 register value 0x%02X\n", byte);
+#endif
+        sound.nr14 = byte;
+        return;
+    case NR21:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR21 register value 0x%02X\n", byte);
+#endif
+        sound.nr21 = byte;
+        return;
+    case NR22:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR22 register value 0x%02X\n", byte);
+#endif
+        sound.nr22 = byte;
+        return;
+    case NR23:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR23 register value 0x%02X\n", byte);
+#endif
+        sound.nr23 = byte;
+        return;
+    case NR24:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR24 register value 0x%02X\n", byte);
+#endif
+        sound.nr24 = byte;
+        return;
+    case NR30:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR30 register value 0x%02X\n", byte);
+#endif
+        sound.nr30 = byte;
+        return;
+    case NR31:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR31 register value 0x%02X\n", byte);
+#endif
+        sound.nr31 = byte;
+        return;
+    case NR32:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR32 register value 0x%02X\n", byte);
+#endif
+        sound.nr32 = byte;
+        return;
+    case NR33:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR33 register value 0x%02X\n", byte);
+#endif
+        sound.nr33 = byte;
+        return;
+    case NR34:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR34 register value 0x%02X\n", byte);
+#endif
+        sound.nr34 = byte;
+        return;
+    case WAV00:
+    case WAV01:
+    case WAV02:
+    case WAV03:
+    case WAV04:
+    case WAV05:
+    case WAV06:
+    case WAV07:
+    case WAV08:
+    case WAV09:
+    case WAV10:
+    case WAV11:
+    case WAV12:
+    case WAV13:
+    case WAV14:
+    case WAV15:
+#ifdef SOUND_LOG
+        write_log("[sound] write to WAX%02d register value 0x%02X\n", addr-WAV00, byte);
+#endif
+        sound.wav[addr-WAV00] = byte;
+        return;
+    case NR41:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR41 register value 0x%02X\n", byte);
+#endif
+        sound.nr41 = byte;
+        return;
+    case NR42:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR42 register value 0x%02X\n", byte);
+#endif
+        sound.nr42 = byte;
+        return;
+    case NR43:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR43 register value 0x%02X\n", byte);
+#endif
+        sound.nr43 = byte;
+        return;
+    case NR44:
+#ifdef SOUND_LOG
+        write_log("[sound] write to NR44 register value 0x%02X\n", byte);
+#endif
+        sound.nr44 = byte;
+        return;
     default:
         write_log("[memory] unimplemented write to I/O port 0x%04X value 0x%02X\n", addr, byte);
         die(-1, NULL);
